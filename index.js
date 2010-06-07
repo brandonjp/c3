d5de9da35df422b94175cc952c9c9ca23c317a73
brandonjp <brandonjp@gmail.com>
2010-06-07 13:27:17 -0700

    changed div column classes to "c3col"

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 jQuery(document).ready(function($) {
-	
+
 	function doStuff() {
 		$('li>ul').hide()  //  hide the ul's contained inside li's
 		.parent('li').click(function() {  //  get the parent li of the hidden ul's and when clicked...
 			var alanthicke = $(this).children('ul').clone().show();  //  set a variable for the ul inside that li, then clone it and show the clone
-			$(this).parents('div.col').next('div.col').html(function() {  //  get this, find the parent div.col, then the next div.col and set the html...
-				$(this).html(alanthicke);  //  take that next div.col and set it's html content to the variable we created on click
+			$(this).parents('div.c3col').next().html(function() {  //  get this, find the parent div.c3col, then the next div.c3col and set the html...
+				$(this).html(alanthicke);  //  take that next div.c3col and set it's html content to the variable we created on click
 				doStuff();  //  then run this function again on the inserted content, this allows us to unlimited nested ul's
-			}).nextAll('div.col').html('');  //  finally, clear the html contents of the next and all following div.col's
+			}).nextAll().html('');  //  finally, clear the html contents of the next and all following div.c3col's
 		});
 	}
 
